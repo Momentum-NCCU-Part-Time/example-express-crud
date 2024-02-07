9a06ec17202b055d32fb926a17fe3bcd4df8a606
Amy Gori <amydgori@gmail.com>
2024-02-07 08:13:01 -0500

    Include notes in bookmark model

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 
 // define the schema for the Bookmark model
-const bookmarkSchema = new mongoose.Schema({
-  title: String,
-  url: String,
-})
+const bookmarkSchema = new mongoose.Schema(
+  {
+    title: String,
+    url: String,
+    notes: [{ text: String }],
+  },
+  { timestamps: true }
+)
 
 // create the model and allow it to be used in other files
 module.exports = mongoose.model('Bookmark', bookmarkSchema)
+
+
